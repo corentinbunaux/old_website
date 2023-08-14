0bbb129afd45a2687f5e9a66e9be5b66f8f459a4
Cocobunaux <corentin.bunaux@gmail.com>
2023-08-14 18:19:39 +0200

    parcours fini

--- path.js
+++ path.js
@@ -3,11 +3,7 @@ const charging = document.querySelector("#charging");
 const contenu = document.querySelector(".contenu");
 const tennis = document.querySelector(".aboutme button");
 const decouvrir = document.getElementById('decouvrir');
-const path_slide_top = document.getElementById("slide_top");
-const path_slide_bottom = document.getElementById("slide_bottom");
-//console.log(path_slide_bottom, path_slide_top);
-
-
+const path_slides = document.querySelectorAll(".slides");
 
 //afficher le contenu selon la colonne surlignée dans projets
 const card = document.querySelector(".card");
@@ -47,7 +43,7 @@ for (let elem = 0; elem < card.children.length; elem++) {
 
 
 //teste l'orientation du site
-var mql = window.matchMedia("(orientation: portrait)");
+/*var mql = window.matchMedia("(orientation: portrait)");
 mql.addListener(handleOrientationChange);
 handleOrientationChange(mql);
 
@@ -57,38 +53,58 @@ function handleOrientationChange(mql) {
     } else {//paysage
     }
 }
+*/
 
+
+function SupprDecouvrir() {
+    decouvrir.style.zIndex = -1;
+}
+
+function ApparaitreSlides() {
+    for (let elem = 0; elem < path_slides.length; elem++) {
+        path_slides[elem].children[0].style.opacity = 1;
+    }
+}
+
+function DisparaitreSlides() {
+    for (let elem = 0; elem < path_slides.length; elem++) {
+        path_slides[elem].children[0].style.opacity = 0;
+    }
+}
 
 //découvre la barre de progression de parcours
 function GenereProgression() {
-    decouvrir.innerHTML = ``;
-    decouvrir.style.zIndex = -1;
+    decouvrir.style.opacity = 0;
     console.log(decouvrir);
-    path_slide_bottom.innerHTML = `                            <div
+    setTimeout(SupprDecouvrir, 500);
+
+    path_slides[1].innerHTML = `                            <div
     class="position-absolute date date_mse fs-5 p-1 rounded-pill d-flex justify-content-center align-items-center">
-    2025 - 2022</div>`;
-    path_slide_top.innerHTML = `<div class="position-absolute formation formation_mse">
-                <h3>École des Mines de Saint-Étienne</h1>
-                    <p>
-                        <strong>I</strong><span>ngénieur</span>
-                        <strong>S</strong><span>pécialité</span>
-                        <strong>M</strong><span>icroélectronique et</span>
-                        <strong>In</strong>formatique<span></span>
-                    </p>
-                    <p>
-                        <span>Rang français :</span> <strong>11e</strong><span>.</span> <span>Rang
-                            mondial :</span> <strong>Top 501-600</strong><span>.</span>
-                    </p>
-                    <button class="cta">
-                        <span>En savoir plus</span>
-                        <svg viewBox="0 0 13 10" height="10px" width="15px">
-                            <path d="M1,5 L11,5"></path>
-                            <polyline points="8 1 12 5 8 9"></polyline>
-                        </svg>
-                    </button>
-            </div>`;
+    2025 - 2022</div>`
+    path_slides[0].innerHTML = `<div class="position-absolute formation formation_mse">
+    <h3>École des Mines de Saint-Étienne</h1>
+        <p>
+            <strong>I</strong><span>ngénieur</span>
+            <strong>S</strong><span>pécialité</span>
+            <strong>M</strong><span>icroélectronique et</span>
+            <strong>In</strong>formatique<span></span>
+        </p>
+        <p>
+            <span>Rang français :</span> <strong>11e</strong><span>.</span> <span>Rang
+                mondial :</span> <strong>Top 501-600</strong><span>.</span>
+        </p>
+        <button class="cta">
+            <span>En savoir plus</span>
+            <svg viewBox="0 0 13 10" height="10px" width="15px">
+                <path d="M1,5 L11,5"></path>
+                <polyline points="8 1 12 5 8 9"></polyline>
+            </svg>
+        </button>
+</div>`;
+
+    setTimeout(ApparaitreSlides, 300);
+
     const cta = document.querySelector(".cta");
-    console.log(cta);
 
     cta.addEventListener("click", function () {
         window.open("https://www.mines-stetienne.fr/formation/ismin/");
@@ -114,7 +130,9 @@ for (let elem = 0; elem < button.length; elem++) {
         //anime les boutons selon celui activé
         for (let i = 0; i <= elem; i++) {
             button[i].classList.add('checked');
-            button[i + 1].classList.add('opacity-100');
+            if (i != button.length - 1) {
+                button[i + 1].classList.add('opacity-100');
+            }
         }
         for (let i = button.length - 1; i > elem; i--) {
             button[i].classList.remove('checked');
@@ -126,29 +144,34 @@ for (let elem = 0; elem < button.length; elem++) {
         switch (elem) {
 
             case 0:
-                path_slide_bottom.innerHTML = `                            <div
-                class="position-absolute date date_mse fs-5 p-1 rounded-pill d-flex justify-content-center align-items-center">
-                2025 - 2022</div>`;
-                path_slide_top.innerHTML = `<div class="position-absolute formation formation_mse">
-                <h3>École des Mines de Saint-Étienne</h1>
-                    <p>
-                        <strong>I</strong><span>ngénieur</span>
-                        <strong>S</strong><span>pécialité</span>
-                        <strong>M</strong><span>icroélectronique et</span>
-                        <strong>In</strong>formatique<span></span>
-                    </p>
-                    <p>
-                        <span>Rang français :</span> <strong>11e</strong><span>.</span> <span>Rang
-                            mondial :</span> <strong>Top 501-600</strong><span>.</span>
-                    </p>
-                    <button class="cta">
-                        <span>En savoir plus</span>
-                        <svg viewBox="0 0 13 10" height="10px" width="15px">
-                            <path d="M1,5 L11,5"></path>
-                            <polyline points="8 1 12 5 8 9"></polyline>
-                        </svg>
-                    </button>
-            </div>`;
+                DisparaitreSlides;
+
+                path_slides[1].innerHTML = `                            <div
+    class="position-absolute date date_mse fs-5 p-1 rounded-pill d-flex justify-content-center align-items-center">
+    2025 - 2022</div>`
+                path_slides[0].innerHTML = `<div class="position-absolute formation formation_mse">
+    <h3>École des Mines de Saint-Étienne</h1>
+        <p>
+            <strong>I</strong><span>ngénieur</span>
+            <strong>S</strong><span>pécialité</span>
+            <strong>M</strong><span>icroélectronique et</span>
+            <strong>In</strong>formatique<span></span>
+        </p>
+        <p>
+            <span>Rang français :</span> <strong>11e</strong><span>.</span> <span>Rang
+                mondial :</span> <strong>Top 501-600</strong><span>.</span>
+        </p>
+        <button class="cta">
+            <span>En savoir plus</span>
+            <svg viewBox="0 0 13 10" height="10px" width="15px">
+                <path d="M1,5 L11,5"></path>
+                <polyline points="8 1 12 5 8 9"></polyline>
+            </svg>
+        </button>
+</div>`;
+
+                setTimeout(ApparaitreSlides, 100);
+
                 const cta = document.querySelector(".cta");
 
                 cta.addEventListener("click", function () {
@@ -157,8 +180,10 @@ for (let elem = 0; elem < button.length; elem++) {
                 break;
 
             case 1:
-                path_slide_bottom.innerHTML = `<div class="position-absolute formation formation_vh">
-                <h3>CPGE - Lycée Victor Hugo ~ Caen</h1>
+                DisparaitreSlides;
+                path_slides[1].innerHTML = `<div class="position-absolute formation formation_vh">
+                <h3>CPGE - Lycée Victor Hugo</h3>
+                <h5>Caen</h5>
                     <p>
                         <strong>P</strong><span>hysique &</span>
                         <strong>S</strong><span>ciences</span>
@@ -169,19 +194,48 @@ for (let elem = 0; elem < button.length; elem++) {
                         <strong>Mines-Telecom</strong><span>.</span>
                     </p>
             </div>`;
-                path_slide_top.innerHTML = `<div
+                path_slides[0].innerHTML = `<div
                 class="position-absolute date date_vh fs-5 p-1 rounded-pill d-flex justify-content-center align-items-center">
                 2022 - 2021</div>`;
+                setTimeout(ApparaitreSlides, 100);
                 break;
 
             case 2:
-                path_slide_bottom.innerHTML = ``;
-                path_slide_top.innerHTML = ``;
+                DisparaitreSlides;
+                path_slides[1].innerHTML = `                            <div
+                class="position-absolute date date_f1 fs-5 p-1 rounded-pill d-flex justify-content-center align-items-center">
+                2021 - 2020</div>`;
+                path_slides[0].innerHTML = `                            <div class="position-absolute formation formation_f1">
+                <h3>CPGE - Lycée François 1er</h3>
+                <h5>Le Havre</h5>
+
+                <p>
+                    <strong>P</strong><span>hysique</span>
+                    <strong>C</strong><span>himie &</span>
+                    <strong>S</strong><span>ciences</span>
+                    <strong>I</strong><span>ndustrielles</span>
+                </p>
+            </div>`;
+                setTimeout(ApparaitreSlides, 100);
                 break;
 
             case 3:
-                path_slide_bottom.innerHTML = ``;
-                path_slide_top.innerHTML = ``;
+                DisparaitreSlides;
+                path_slides[1].innerHTML = `<div class="position-absolute formation formation_glc">
+                <h3>Lycée Guillaume Le Conquérant</h3>
+                <h5>Lillebonne</h5>
+                <p>
+                    <span>Baccalauréat série</span> <strong>S</strong><span>cientifique</span> <span>-
+                        option SVT,</span>
+                </p>
+                <p>
+                    <span>Admis mention</span> <strong>Très Bien</strong>
+                </p>
+            </div>`;
+                path_slides[0].innerHTML = `<div
+                class="position-absolute date date_glc fs-5 p-1 rounded-pill d-flex justify-content-center align-items-center">
+                2020 - 2017</div>`;
+                setTimeout(ApparaitreSlides, 100);
                 break;
         }
     })
